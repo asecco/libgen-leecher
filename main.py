a68f3bb1f8d835d0b06f21f5909d8d4f33f0b879
Andrew Secco <asecco99@gmail.com>
2022-02-08 20:07:11 -0500

    Looped script to follow if statement

--- main.py
+++ main.py
@@ -2,17 +2,7 @@ from libgen_api import LibgenSearch
 import urllib.request
 import pprint
 
-book = str(input("What is the book title?: "))
-
-s = LibgenSearch()
-title_filters = {"Extension": "pdf"}
-results = s.search_title_filtered(book, title_filters, exact_match=True)
-pp = pprint.PrettyPrinter(indent=2)
-pp.pprint(results)
-
-item_to_download = results[0]
-download_links = s.resolve_download_links(item_to_download)
-url = download_links["Cloudflare"]
+print("Welcome to LibGen Leecher\n")
 
 def main():
     download_file(url)
@@ -22,6 +12,30 @@ def download_file(url):
     file = open(book + ".pdf", 'wb')
     file.write(response.read())
     file.close()
-    print("Completed")
+    print("\nDownload complete. Enjoy!")
 
-main()
+def search():
+    global book
+    book = str(input("What is the book title? "))
+    s = LibgenSearch()
+    title_filters = {"Extension": "pdf"}
+    results = s.search_title_filtered(book, title_filters, exact_match=True)
+    pp = pprint.PrettyPrinter(indent=2)
+    print("\nResults: \n")
+    pp.pprint(results)
+
+    item_to_download = results[0]
+    download_links = s.resolve_download_links(item_to_download)
+    global url
+    url = download_links["Cloudflare"]
+
+    correct_book = str(input("\nIs this the book you're looking for? [y/n] "))
+    if correct_book == "y":
+        main()
+    elif correct_book == "n":
+        print("\nPlease refine your search\n")
+        search()
+    else:
+        print("\nInvalid input. \nExiting.")
+        quit()
+search()
