fc9028246d51ce0e6a5c5d0aa99a3d6d3d5a0bb2
Andrew Secco <asecco99@gmail.com>
2022-02-27 15:07:30 -0500

    Updated setup.py entry points

--- setup.py
+++ setup.py
@@ -4,10 +4,13 @@ setup(
     name = "LibGen-Leecher",
     description = 'Library Genesis downloader',
     long_description = 'A script for easily downloading books from Library Genesis.',
-    version = '0.1.1',
+    version = '0.0.1',
     author = "Andrew Secco",
     author_email = "<asecco99@gmail.com>",
     packages = find_packages(),
+    entry_points = {
+        'console_scripts': ['libGen-leecher=LibGenLeecher:search']
+        },
     install_requires = ['libgen-api'],
     keywords = ['python', 'downloader', 'library genesis', 'textbooks'],
 )
